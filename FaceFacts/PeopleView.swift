c6f9128f8e9874312c43359fa6473d2aad3442ce
tromao <tiagomromao@gmail.com>
2024-01-13 20:57:22 +0000

    Update README.md

---
+++ FaceFacts/PeopleView.swift
@@ -0,0 +1,18 @@
+//
+//  PeopleView.swift
+//  FaceFacts
+//
+//  Created by Tiago Miguel de Jesus Romao on 13/01/2024.
+//
+
+import SwiftUI
+
+struct PeopleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PeopleView()
+}
