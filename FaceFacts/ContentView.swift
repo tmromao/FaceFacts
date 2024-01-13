220f08ca8eca1b090ecdb9023b6f1a0c095daba3
tromao <tiagomromao@gmail.com>
2024-01-13 22:23:18 +0000

    added people view

--- FaceFacts/ContentView.swift
+++ FaceFacts/ContentView.swift
@@ -11,26 +11,21 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @State private var path = [Person]()
-    @Query var people: [Person]
     
+    @State private var searchText = ""
     
     
     var body: some View {
         NavigationStack(path: $path){
-            List{
-                ForEach(people){ person in
-                    NavigationLink(value: person){
-                        Text(person.name)
-                    }
+            PeopleView()
+                .navigationTitle("FaceFacts")
+                .navigationDestination(for: Person.self){ person in
+                    EditPersonView(person: person)
                 }
-            }
-            .navigationTitle("FaceFacts")
-            .navigationDestination(for: Person.self){ person in
-                EditPersonView(person: person)
-            }
-            .toolbar{
-                Button("Add Peron", systemImage: "plus", action: addPerson)
-            }
+                .toolbar{
+                    Button("Add Peron", systemImage: "plus", action: addPerson)
+                }
+                .searchable(text: $searchText)
         }
     }
     
--- FaceFacts/PeopleView.swift
+++ FaceFacts/PeopleView.swift
@@ -5,11 +5,29 @@
 //  Created by Tiago Miguel de Jesus Romao on 13/01/2024.
 //
 
+import SwiftData
 import SwiftUI
 
 struct PeopleView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var people: [Person]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List{
+            ForEach(people){ person in
+                NavigationLink(value: person){
+                    Text(person.name)
+                }
+            }
+            .onDelete(perform: deletePeople)
+        }
+    }
+    
+    func deletePeople(at offsets: IndexSet){
+        for offset in offsets {
+            let person = people[offset]
+            modelContext.delete(person)
+        }
     }
 }
 
